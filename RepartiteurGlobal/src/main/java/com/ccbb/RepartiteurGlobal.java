0dea8adfb58515728b82c1bbb0d747a0f295c58e
CCA3441 <clement.carretier@gmail.com>
2016-05-09 10:49:15 +0200

    branch

--- RepartiteurGlobal/src/main/java/com/ccbb/RepartiteurGlobal.java
+++ RepartiteurGlobal/src/main/java/com/ccbb/RepartiteurGlobal.java
@@ -1,6 +1,8 @@
 package com.ccbb;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
@@ -11,7 +13,7 @@ public class RepartiteurGlobal {
 
     public static void main(String[] args) throws Exception {
         String s;
-        process = Runtime.getRuntime().exec("nova boot --flavor m1.small --image FC23 --nic" +
+        executeProcess("nova boot --flavor m1.small --image FC23 --nic" +
                 "net-id=c1445469-4640-4c5a-ad86-9c0cb6650cca --security-group default" +
                 "--key-name mykeyCCBB private-instanceCCBB");
         BufferedReader br = new BufferedReader(
@@ -21,6 +23,35 @@ public class RepartiteurGlobal {
         process.waitFor();
         System.out.println ("exit: " + process.exitValue());
         process.destroy();
+    }
+
+    public static String executeProcess(String cmd) {
+        ProcessBuilder process = new ProcessBuilder("/bin/sh", "-c", cmd);
+
+        Process p;
+        StringBuilder sb = new StringBuilder();
+        try {
+            p = process.start();
+
+            try {
+                p.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            InputStream is = p.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String ligne;
+
+            while (( ligne = br.readLine()) != null) {
+                sb.append(ligne).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
     }
 
 }
