163d6672a1d2ae639acd992e61c0705c1caac75f
CCA3441 <clement.carretier@gmail.com>
2016-05-11 14:21:09 +0200

    commit

--- test/src/main/java/com/ccbb/Main.java
+++ test/src/main/java/com/ccbb/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        File f = new File("test.txt");
+        File f = new File("/home/ubuntu/test.txt");
         OutputStream inputStream = new FileOutputStream(f);
         inputStream.write(("Salut c'est cool").getBytes());
     }
