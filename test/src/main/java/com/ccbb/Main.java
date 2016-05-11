a640922288037b1bed8bd12953b3530ee53b080e
CCA3441 <clement.carretier@gmail.com>
2016-05-11 16:35:21 +0200

    commit

--- test/src/main/java/com/ccbb/Main.java
+++ test/src/main/java/com/ccbb/Main.java
@@ -9,8 +9,19 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        File f = new File("/home/fedora/test.txt");
-        OutputStream inputStream = new FileOutputStream(f);
-        inputStream.write(("Salut c'est cool").getBytes());
+        File f = new File("/home/fedora/text.txt");
+        int cpt = 0;
+        while(f.exists()) {
+            File file;
+            if(cpt < 10){
+                file = new File("/home/fedora/tempo"+cpt+".txt");
+                cpt ++;
+            } else {
+                cpt --;
+                file = new File("/home/fedora/tempo"+cpt+".txt");
+                file.delete();
+            }
+            f = new File("/home/fedora/text.txt");
+        }
     }
 }
