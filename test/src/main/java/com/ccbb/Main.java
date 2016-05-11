b0c3e624e700d62a78796e5d83fbf853151a2e8a
CCA3441 <clement.carretier@gmail.com>
2016-05-11 16:42:45 +0200

    commit

--- test/src/main/java/com/ccbb/Main.java
+++ test/src/main/java/com/ccbb/Main.java
@@ -9,19 +9,8 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        File f = new File("/home/fedora/text.txt");
-        int cpt = 0;
-        while(f.exists()) {
-            File file;
-            if(cpt < 1){
-                file = new File("/home/fedora/tempo"+cpt+".txt");
-                cpt ++;
-            } else {
-                cpt --;
-                file = new File("/home/fedora/tempo"+cpt+".txt");
-                file.delete();
-            }
+            System.out.println("Salut");
             Thread.currentThread().sleep(10000);
-        }
+
     }
 }
