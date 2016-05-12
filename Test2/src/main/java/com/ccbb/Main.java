7ea4a0d4d2fc7141ae1b422a67c0056351d3af22
CCA3441 <clement.carretier@gmail.com>
2016-05-12 10:36:07 +0200

    commit

--- Test2/src/main/java/com/ccbb/Main.java
+++ Test2/src/main/java/com/ccbb/Main.java
@@ -13,7 +13,7 @@ import java.net.URL;
  */
 public class Main {
     private static final String url = "http://";
-    private static final String adresseDestination = "localhost";
+    private static final String adresseDestination = "192.168.0.74";
     private static final String portDestination = "2000";
     private static final String root = "/test";
 
