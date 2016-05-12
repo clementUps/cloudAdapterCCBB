5a2c9e217cedd0ffeddb359983aad06e6a57d59a
CCA3441 <clement.carretier@gmail.com>
2016-05-12 16:39:30 +0200

    commit

--- Test2/src/main/java/com/ccbb/Main.java
+++ Test2/src/main/java/com/ccbb/Main.java
@@ -8,17 +8,25 @@ import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
 import java.io.IOException;
 import java.net.URL;
 
+import org.openstack4j.api.OSClient;
+import org.openstack4j.model.network.IPVersionType;
+import org.openstack4j.model.network.Network;
+import org.openstack4j.model.network.Subnet;
+import org.openstack4j.openstack.OSFactory;
+import org.openstack4j.openstack.networking.domain.NeutronNetwork;
+import org.openstack4j.openstack.networking.domain.NeutronSubnet;
+
 /**
  * Created by CCA3441 on 11/05/2016.
  */
 public class Main {
     private static final String url = "http://";
-    private static final String adresseDestination = "192.168.0.77";
+    private static final String adresseDestination = "192.168.56.1";
     private static final String portDestination = "2001";
     private static final String root = "/test";
 
     public static void main(String[] args) throws IOException, InterruptedException, XmlRpcException {
-        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+       /* XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
         config.setServerURL(new URL(url.concat(adresseDestination).concat(":").concat(String.valueOf(portDestination)).concat(root)));
         config.setEnabledForExtensions(true);
         config.setConnectionTimeout(60 * 1000);
@@ -32,7 +40,18 @@ public class Main {
         client.setConfig(config);
         final Object[] params = new Object[]
                 { new String(" nombre ")};
-        System.out.println(client.execute("A.bonjour", params));
+        System.out.println(client.execute("A.bonjour", params));*/
+
+
+
+
+                OSClient os = OSFactory.builder()
+                        .endpoint("http://195.220.53.63:1500/v2.0")
+                        .credentials("ens11","J8N9CE").tenantName("service")
+                        .authenticate();
+                System.out.println(os);
+                System.out.println(os.images().list());
+
     }
 
 }
