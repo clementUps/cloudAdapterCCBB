4f90f791f232642b71959513c108cfdf4c34fa32
CCA3441 <clement.carretier@gmail.com>
2016-05-13 10:11:37 +0200

    commit

--- startupServer.sh
+++ startupServer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-java -jar /home/fedora/projet/cloudAdapterCCBB/repartiteur/repartiteur-jar-with-dependencies.jar 2003
-java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8081
-java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8082
-java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8083
-java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8084
-java -jar /home/fedora/projet/cloudAdapterCCBB/updateRepartieur/updateRepartieur-jar-with-dependencies.jar localhost 2003 add localhost 8081
-java -jar /home/fedora/projet/cloudAdapterCCBB/updateRepartieur/updateRepartieur-jar-with-dependencies.jar localhost 2003 add localhost 8082
-java -jar /home/fedora/projet/cloudAdapterCCBB/updateRepartieur/updateRepartieur-jar-with-dependencies.jar localhost 2003 add localhost 8083
-java -jar /home/fedora/projet/cloudAdapterCCBB/updateRepartieur/updateRepartieur-jar-with-dependencies.jar localhost 2003 add localhost 8084
+java -jar /home/fedora/projet/cloudAdapterCCBB/repartiteur/repartiteur-jar-with-dependencies.jar 2003 &
+java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8081 &
+java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8082 &
+java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8083 &
+java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8084 &
+java -jar /home/fedora/projet/cloudAdapterCCBB/updateRepartieur/updateRepartiteur-jar-with-dependencies.jar localhost 2003 add localhost 8081 &
+java -jar /home/fedora/projet/cloudAdapterCCBB/updateRepartieur/updateRepartiteur-jar-with-dependencies.jar localhost 2003 add localhost 8082 &
+java -jar /home/fedora/projet/cloudAdapterCCBB/updateRepartieur/updateRepartiteur-jar-with-dependencies.jar localhost 2003 add localhost 8083 &
+java -jar /home/fedora/projet/cloudAdapterCCBB/updateRepartieur/updateRepartiteur-jar-with-dependencies.jar localhost 2003 add localhost 8084 &
