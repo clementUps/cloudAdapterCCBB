7b9e153a3ae0fb17bb8d534cac850698e4d35c50
CCA3441 <clement.carretier@gmail.com>
2016-05-13 09:10:47 +0200

    commit

--- Test2/src/main/java/com/ccbb/Main.java
+++ Test2/src/main/java/com/ccbb/Main.java
@@ -5,21 +5,8 @@ import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
-
-import org.openstack4j.api.OSClient;
-import org.openstack4j.core.transport.Config;
-import org.openstack4j.core.transport.ProxyHost;
-import org.openstack4j.model.network.IPVersionType;
-import org.openstack4j.model.network.Network;
-import org.openstack4j.model.network.Subnet;
-import org.openstack4j.openstack.OSFactory;
-import org.openstack4j.openstack.networking.domain.NeutronNetwork;
-import org.openstack4j.openstack.networking.domain.NeutronSubnet;
 
 /**
  * Created by CCA3441 on 11/05/2016.
@@ -48,36 +35,5 @@ public class Main {
         System.out.println(client.execute("A.bonjour", params));
     }
 
-    public static String executeProcess(String cmd) {
-        ProcessBuilder process = new ProcessBuilder("/bin/sh", "-c", cmd);
-        System.out.println("début ");
-        Process p;
-        StringBuilder sb = new StringBuilder();
-        try {
-            System.out.println("start ");
-            p = process.start();
-            System.out.println("lancé ");
-            try {
-                System.out.println("début wait ");
-                p.waitFor();
-                System.out.println("fin wait ");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            InputStream is = p.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String ligne;
-
-            while (( ligne = br.readLine()) != null) {
-                sb.append(ligne).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return sb.toString();
-    }
 
 }
--- test/src/main/java/com/ccbb/Main.java
+++ test/src/main/java/com/ccbb/Main.java
@@ -1,22 +1,52 @@
 package com.ccbb;
 
 import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
 import org.apache.xmlrpc.server.XmlRpcServer;
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
 import org.apache.xmlrpc.webserver.WebServer;
+import org.openstack4j.api.OSClient;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.URL;
 
 /**
  * Created by CCA3441 on 11/05/2016.
  */
 public class Main {
+    private static final String url = "http://";
+   // private static final String adresseDestination = "127.0.0.1";
+    private static final String portDestination = "2003";
+    private static final String root = "/test";
 
+    public static void sendRequest(String adresseDestination) throws IOException, InterruptedException, XmlRpcException {
+        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+        config.setServerURL(new URL(url.concat(adresseDestination).concat(":").concat(String.valueOf(portDestination)).concat(root)));
+        config.setEnabledForExtensions(true);
+        config.setConnectionTimeout(60 * 1000);
+        config.setReplyTimeout(60 * 1000);
+        final XmlRpcClient client = new XmlRpcClient();
 
+        // use Commons HttpClient as transport
+        client.setTransportFactory(
+                new XmlRpcCommonsTransportFactory(client));
+        // set configuration
+        client.setConfig(config);
+        final Object[] params = new Object[]
+                { new String(" nombre ")};
+        System.out.println(client.execute("A.bonjour", params));
+
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException, XmlRpcException {
+        if(args.length == 1) {
+            sendRequest(args[0]);
+        }
+
         System.out.println("Salut");
         InetAddress adrLocale = InetAddress.getLocalHost();
         System.out.println(" "+adrLocale.getHostAddress());
