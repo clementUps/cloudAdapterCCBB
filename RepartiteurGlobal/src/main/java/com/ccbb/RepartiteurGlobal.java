54709f0dfce9b4616581eb514a76bc1f20bb9dbc
CCA3441 <clement.carretier@gmail.com>
2016-05-13 16:03:15 +0200

    commit

--- RepartiteurGlobal/src/main/java/com/ccbb/RepartiteurGlobal.java
+++ RepartiteurGlobal/src/main/java/com/ccbb/RepartiteurGlobal.java
@@ -5,6 +5,11 @@ import org.apache.xmlrpc.server.PropertyHandlerMapping;
 import org.apache.xmlrpc.server.XmlRpcServer;
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
 import org.apache.xmlrpc.webserver.WebServer;
+import org.openstack4j.api.Builders;
+import org.openstack4j.api.OSClient;
+import org.openstack4j.model.compute.Server;
+import org.openstack4j.model.compute.ServerCreate;
+import org.openstack4j.openstack.OSFactory;
 
 import java.io.IOException;
 
@@ -12,30 +17,16 @@ import java.io.IOException;
  * Created by CCA3441 on 09/05/2016.
  */
 public class RepartiteurGlobal {
-    private static Process process;
 
     public static void main(String[] args) throws Exception {
-        init();
-        RepartiteurUpdate repartiteurUpdate = new RepartiteurUpdate();
-        repartiteurUpdate.addVM();
-    }
-
-    private static void init() throws IOException, XmlRpcException {
-        WebServer webServer = new WebServer(2000);
-
-        XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer();
-
-        PropertyHandlerMapping phm = new PropertyHandlerMapping();
-        phm.load(Thread.currentThread().getContextClassLoader(),
-                "XmlRpcServlet.properties");
-        xmlRpcServer.setHandlerMapping(phm);
-
-        XmlRpcServerConfigImpl serverConfig =
-                (XmlRpcServerConfigImpl) xmlRpcServer.getConfig();
-        serverConfig.setEnabledForExtensions(true);
-        serverConfig.setContentLengthOptional(false);
-        System.out.println("fin init server");
-        webServer.start();
+        ServerCreate sc = Builders.server().name("privateCCBLInstance").image("ccbbImage").build();
+        OSClient os = OSFactory.builder()
+                .endpoint("http://127.0.0.1:5000/v2.0")
+                .credentials("ens11", "J8N9CE")
+                .tenantName("service")
+                .authenticate();
+        Server server = os.compute().servers().boot(sc);
+        System.out.print(server.getAccessIPv4());
     }
 
 }
