c0e7384ecbed5074c33e53d091de0e2f739ff016
CCA3441 <clement.carretier@gmail.com>
2016-05-13 16:07:42 +0200

    commit

--- RepartiteurGlobal/src/main/java/com/ccbb/RepartiteurGlobal.java
+++ RepartiteurGlobal/src/main/java/com/ccbb/RepartiteurGlobal.java
@@ -7,6 +7,7 @@ import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
 import org.apache.xmlrpc.webserver.WebServer;
 import org.openstack4j.api.Builders;
 import org.openstack4j.api.OSClient;
+import org.openstack4j.model.compute.Flavor;
 import org.openstack4j.model.compute.Server;
 import org.openstack4j.model.compute.ServerCreate;
 import org.openstack4j.openstack.OSFactory;
@@ -19,12 +20,21 @@ import java.io.IOException;
 public class RepartiteurGlobal {
 
     public static void main(String[] args) throws Exception {
-        ServerCreate sc = Builders.server().name("privateCCBLInstance").image("ccbbImage").build();
-        OSClient os = OSFactory.builder()
+           OSClient os = OSFactory.builder()
                 .endpoint("http://127.0.0.1:5000/v2.0")
                 .credentials("ens11", "J8N9CE")
                 .tenantName("service")
                 .authenticate();
+        Flavor flavor = Builders.flavor()
+                .name("Large Resources Template")
+                .ram(4096)
+                .vcpus(6)
+                .disk(120)
+                .build();
+
+        flavor = os.compute().flavors().create(flavor);
+        ServerCreate sc = Builders.server().name("privateCCBLInstance").flavor(flavor).image("ccbbImage").build();
+
         Server server = os.compute().servers().boot(sc);
         System.out.print(server.getAccessIPv4());
     }
