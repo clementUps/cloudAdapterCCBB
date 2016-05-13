14e5ee8f36e1ff4e7f1e0a67ea6cc6a72f0225df
CCA3441 <clement.carretier@gmail.com>
2016-05-13 09:55:43 +0200

    commit

--- startupServer.sh
+++ startupServer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rjava -jar /home/fedora/projet/cloudAdapterCCBB/repartiteur/repartiteur-jar-with-dependencies.jar 2003
+java -jar /home/fedora/projet/cloudAdapterCCBB/repartiteur/repartiteur-jar-with-dependencies.jar 2003
 java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8081
 java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8082
 java -jar /home/fedora/projet/cloudAdapterCCBB/server/server-jar-with-dependencies.jar 8083
