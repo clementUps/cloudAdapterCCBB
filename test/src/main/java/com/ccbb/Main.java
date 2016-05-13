c1c395e066bdcc688817e4b5c79e97028a65a4c2
CCA3441 <clement.carretier@gmail.com>
2016-05-13 08:53:37 +0200

    commit

--- test/src/main/java/com/ccbb/Main.java
+++ test/src/main/java/com/ccbb/Main.java
@@ -6,32 +6,17 @@ import org.apache.xmlrpc.server.XmlRpcServer;
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
 import org.apache.xmlrpc.webserver.WebServer;
 
-import java.io.*;
-import java.net.Inet4Address;
+import java.io.IOException;
 import java.net.InetAddress;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * Created by CCA3441 on 11/05/2016.
  */
 public class Main {
 
-     static  Timer timer = new Timer();
-     static TimerTask exitApp = new TimerTask() {
-        public void run() {
-            System.exit(0);
-        }
-    };
-
-    public static void timedExit() {
-        timer.schedule(exitApp, new Date(System.currentTimeMillis()+5*1000));
-    }
 
 
     public static void main(String[] args) throws IOException, InterruptedException, XmlRpcException {
-        timedExit();
         System.out.println("Salut");
         InetAddress adrLocale = InetAddress.getLocalHost();
         System.out.println(" "+adrLocale.getHostAddress());
